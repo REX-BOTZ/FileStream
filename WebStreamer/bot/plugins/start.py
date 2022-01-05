4202cb9a8f9690feba1b66c9b96ac6b7341bdb5c
REX BOTZ <89206214+REX-BOTZ@users.noreply.github.com>
2022-01-05 22:26:22 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -4,8 +4,13 @@ from pyrogram import filters, emoji
 from WebStreamer.bot import StreamBot
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
-@StreamBot.on_message(filters.command(['start', 'help']))
+@StreamBot.on_message(filters.command(['start']))
 async def start(_, m: Message):
     await m.reply(f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.',
                 
+                  )
+
+@StreamBot.on_message(filters.command(['help']))
+async def start(_, m: Message):
+    await m.reply(f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link. \nI am Fastest Download Link Generator Bot. \nJoin :- @Rex_Botz',
                   )
