bf1836ad83f6e2b24438bfe63f110e1714605da9
REX BOTZ <89206214+REX-BOTZ@users.noreply.github.com>
2022-01-05 22:31:37 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -7,7 +7,19 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 @StreamBot.on_message(filters.command(['start']))
 async def start(_, m: Message):
     await m.reply(f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.',
-                
+         disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("ʙᴀᴄᴋ", callback_data="help_data"),
+                    InlineKeyboardButton("sᴛᴀʀᴛ", callback_data="start_data"),
+                ],
+                [
+                    InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/Benwolf24")
+                ]
+            ]
+        ),
+        reply_to_message_id=message.message_id       
                   )
 
 @StreamBot.on_message(filters.command(['help']))
