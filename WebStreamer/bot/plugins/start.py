558b3fdf6bb0a0f5c796861067cc784742740017
REX BOTZ <89206214+REX-BOTZ@users.noreply.github.com>
2022-01-06 07:32:11 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -1,4 +1,4 @@
-
+#RexBotz #Benwolf #Changing Codes May Cause Some Problem
 
 from pyrogram import filters, emoji
 from WebStreamer.bot import StreamBot
@@ -11,11 +11,11 @@ async def start(_, m: Message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('ᴄʜᴀɴɴᴇʟ', url='https://t.me/Rex_Botz'),
+                    InlineKeyboardButton('ᴄʜᴀɴɴᴇʟ', url='https://t.me/Rex_Botz'),#Give Credits to Us.
                     InlineKeyboardButton('ɢʀᴏᴜᴘ', url='https://t.me/Rex_Bots_Support')
                 ],
                 [
-                    InlineKeyboardButton('ᴅᴇᴠᴇʟᴏᴘᴇʀ', url='https://t.me/Benwolf24'),
+                    InlineKeyboardButton('ᴅᴇᴠᴇʟᴏᴘᴇʀ', url='https://t.me/Benwolf24'),#Atleast Don't Change this. #Give Credits to @Benwolf24
                     InlineKeyboardButton('ᴏᴛʜᴇʀ ʙᴏᴛs', url='https://t.me/Rex_Botz/94/')
                 ]
             ]
@@ -31,6 +31,24 @@ async def start(_, m: Message):
                 [
                     InlineKeyboardButton('ᴘᴏᴡᴇʀᴇᴅ ʙʏ', url='https://t.me/FluxPlay'),
                     InlineKeyboardButton('ɢʀᴏᴜᴘ', url='https://t.me/Rex_Bots_Support')
+                ],
+                [
+                    InlineKeyboardButton('ᴅᴇᴠᴇʟᴏᴘᴇʀ', url='https://t.me/Benwolf24'),
+                    InlineKeyboardButton('ᴏᴛʜᴇʀ ʙᴏᴛs', url='https://t.me/Rex_Botz/94/')
+                ]
+            ]
+        )
+                 )
+
+@StreamBot.on_message(filters.command(['about']))
+async def start(_, m: Message):
+    await m.reply(f'Hi {m.from_user.mention(style="md")}, \n✪ Bot Name : StreamLinkRex \n✪ Developer : @Benwolf24 \n✪ Support Channel : @Rex_Botz \n✪ Group : @Rex_Bots_Support \n✪ Powered By : @FluxPlay \n\n© @Rex_Botz \nHelp @Rex_Bots_Support',
+                 quote=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton('ʟᴇᴇᴄʜ ɢʀᴏᴜᴘ', url='https://t.me/+XovvU7Ri0p5hMDk1'),
+                    InlineKeyboardButton('ʜᴇʟᴘ', url='https://t.me/Rex_Bots_Support')
                 ],
                 [
                     InlineKeyboardButton('ᴅᴇᴠᴇʟᴏᴘᴇʀ', url='https://t.me/Benwolf24'),
